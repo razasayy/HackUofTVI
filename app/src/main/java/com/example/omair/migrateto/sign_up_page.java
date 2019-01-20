9c94cd247fa54224f8bcb5d50322a3e9cf1cc77a
Vanessa Seto <vanessaseto@Vanessas-MacBook-Air.local>
2019-01-20 05:20:06 -0500

    New updates to log in, update information

---
+++ app/src/main/java/com/example/omair/migrateto/sign_up_page.java
@@ -0,0 +1,13 @@
+package com.example.omair.migrateto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sign_up_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+}
